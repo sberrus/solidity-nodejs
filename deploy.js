0b9d02fda6d14583816d18373b37f4c6382f1c75
samdev <samuelberrus@gmail.com>
2022-06-19 03:11:30 +0200

    Enviando transacción enviando los datos a mano [ineficiente preferible usar ethers]

--- deploy.js
+++ deploy.js
@@ -23,26 +23,37 @@ const main = async () => {
 
 	// Utilizamos el método deploy() que como indica, realiza el deploy del contrato a la red.
 	try {
-		// Si todo sale correctamente, veremos que el contrato ha sido enviado correctamente a la red y
-		// contractFactory nos devuelve una instancia del contrato desplegado en la red.
-		const contract = await contractFactory.deploy();
+		// // Si todo sale correctamente, veremos que el contrato ha sido enviado correctamente a la red y
+		// // contractFactory nos devuelve una instancia del contrato desplegado en la red.
+		// const contract = await contractFactory.deploy();
 
-		// Podemos configurar que se de por válido el deploy tomando en cuenta la cantidad de bloques que se han validado
+		// // Podemos configurar que se de por válido el deploy tomando en cuenta la cantidad de bloques que se han validado.
+		// // Con esta línea de código estamos indicando que espere que mínimo se confirme la transacción en un bloque antes de darla por válida.
+		// const deploymentReceipt = await contract.deployTransaction.wait(1);
+		console.log("lets deploy with only transaction data!");
+		const nonce = await wallet.getTransactionCount();
+		const tx = {
+			// nonce es el recuento de transacciones que tiene una wallet. Esto se usa como prevención para comprobar que no se esta enviando
+			// por error 2 veces la misma transacción.
+			// Si llegase a pasar, daría un error debido a que los nonce no coincidirián y una de las transacciones fallaría.
+			//
+			nonce,
+			gasPrice: 20000000000,
+			// El gas limit nos permite poner un tope de gas a una transacción. De manera que podamos controlar que no se nos
+			// vaya de presupuesto realizar alguna transacción por su gas.
+			gasLimit: 1000000,
+			// hashcode del contrato o wallet receiver
+			to: null,
+			// wei que se va a enviar
+			value: 0,
+			// bin del contrato que quermeos desplegar a la red
+			data: null,
+			chainId: 1337,
+		};
 
-		// Con esta línea de código estamos indicando que espere que mínimo se confirme la transacción en un bloque antes de darla por válida.
-		const deploymentReceipt = await contract.deployTransaction.wait(1);
-
-		// La transacción es la información acerca del contrato o la transacción que se ha realizado.
-		console.log("======================");
-		console.log("Deployment Transaction");
-		console.log("======================");
-		console.log(contract);
-
-		// El recibo es la confirmación que hemos declarado para que se válide esta transacción
-		console.log("==================");
-		console.log("Deployment Receipt");
-		console.log("==================");
-		console.log(deploymentReceipt);
+		const sentTxResponse = await wallet.sendTransaction(tx);
+		sentTxResponse.wait(1);
+		console.log(sentTxResponse);
 	} catch (error) {
 		throw new Error(error);
 	}
