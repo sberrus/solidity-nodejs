1cbda1847f10cba8e40d334333309dbb6097d28c
samdev <samuelberrus@gmail.com>
2022-06-20 16:29:28 +0200

    leyendo variables del contrato y utilizando métodos del mismo - 0:7:33:00

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 En esta sección nos vamos a encargar de configurar todo el entorno de Node para que podamos compilar y testear los contratos antes de que se vayan a enviar a la red y que podamos tener mayor control sobre las acciones que se vayan a hacer en el compilador, además de poder apoyarnos más con el editor de VSCode.
 
--- 6:50:00 -> Clases sobre la configuración
+### IMPORTANTE
+
+A partir del tiempo 7:30:00 del curso, en los commits de git aparecerá el minuto el cual se ha dejado el video al realizar el commit de manera que se pueda llevar mejor las anotaciones del curso.
--- deploy.js
+++ deploy.js
@@ -8,10 +8,14 @@ const main = async () => {
 	const provider = new ethers.providers.JsonRpcProvider("http://127.0.0.1:8545");
 
 	// privateKey de wallet local de Ganache.
-	const ganacheWalletPrivateKey = "5602bbc0403c001b84e792d62419d44badc67ad6b36ef960a78d7567fb809bfc";
+	const ganacheSenderPrivateKey = "0678ba9fffbc54e4fe9cc4cb683b0f1e33e5a39f7209cec7edf845543a0012a1";
+	/*								 ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+									 CADA VEZ QUE SE REINICIE EL SERVIDOR DE GANACHE, HAY QUE MODIFICAR LA
+									 PRIVATE KEY DEL SENDER.
+	*/
 
 	// Instanciando un wallet de ethers. Pasamos la private key
-	const wallet = new ethers.Wallet(ganacheWalletPrivateKey, provider);
+	const wallet = new ethers.Wallet(ganacheSenderPrivateKey, provider);
 
 	// Obtenemos el abi y el binary del contrato ya compilado que queremos enviar a la red.
 	const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf-8");
@@ -19,7 +23,7 @@ const main = async () => {
 
 	// El objeto contract factory es el que nos permite realizar el deploy a la red.
 	const contractFactory = new ethers.ContractFactory(abi, bin, wallet);
-	console.log("Desplegando contrato espere...");
+	console.log("Desplegando contrato espere... ⏳");
 
 	try {
 		// Utilizamos el método deploy() que como indica, realiza el deploy del contrato a la red.
@@ -35,9 +39,46 @@ const main = async () => {
 		// Ya estando el contrato en la red, la instancia "contract" contiene los métodos y variables que tiene el contrato
 		// Por lo que podemos utilizar sus métodos ya que el ABI le dice que cosas deben comportarse de cierta manera.
 		const contractFavoriteNumber = await contract.retrieves();
-		console.log(contractFavoriteNumber);
+		console.log({
+			msg: "Respuesta en formato plano",
+			res: contractFavoriteNumber,
+		}); // -> BigNumber { _hex: '0x00', _isBigNumber: true }
+
+		/**
+		 * BIG NUMBERS:
+		 * Javascript tiene una limitación a la hora de trabajar con números muy grandes. Sin entrar en aspcetos muy técnicos podriamos decir que con
+		 * los números de javascript, solo podriamos llegar hasta ~0.0009 wei que es el número que alcanzaría el Number.MAX_SAFE_INTEGER.
+		 * Como los wei son números 1 * 1e18 debemos utilizar una libreria que ya incluye ethers que nos ayuda a trabajar con las respuestas
+		 * hex que nos devuelven ciertos valores de solidity.
+		 *
+		 * Una de las formas que tenemos para poder acceder a el número en formato numerico es utilizar el método .toString() en la respuesta del contrato.
+		 */
+
+		console.log({
+			msg: "Respuesta con .toString()",
+			res: contractFavoriteNumber.toString(),
+		}); // -> BigNumber { _hex: '0x00', _isBigNumber: true }
+
+		/**
+		 * LLAMANDO A FUNCIONES DEL CONTRATO.
+		 * Al igual que podemos acceder a las variables públicas del contrato como propiedades de la instancia del mismo, también podemos llamar a sus
+		 * funciones de la siguiente manera:
+		 */
+		console.log("Actualizando favorite number del contrato... ⏳");
+		const transactionResponse = await contract.store("1225");
+		/**												  ^^^^
+		 * 												  Como se puede observar aquí, se envia en formato string por el mismo problema
+		 * 												  de los bignumbers que hemos mencionado anteriormente.
+		 */
+		const transactionReceipt = await transactionResponse.wait(1); // Esperamos que la transacción se confirme en almenos 1 bloque antes de validarla.
+		console.log("Contrato actualizado con exito ✅");
+		const updatedFavoriteNumber = await contract.retrieves();
+		console.log(`El número favorito actual de este contrato es: ${updatedFavoriteNumber}`);
 	} catch (error) {
-		throw new Error(error);
+		error.code === "SERVER_ERROR" &&
+			console.log(
+				"Error al realizar deploy.\n\n Si esta intentando realizar el deploy en una red de pruebas compruebe la private key del sender \n\n"
+			);
 	}
 };
 
